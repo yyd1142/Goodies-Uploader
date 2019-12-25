2a7dfeff505325fb74488622e92b49fd1f9ea214
YYD <yyd-yyd@hotmail.com>
2019-12-25 23:59:32 +0800

    change something

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-window.fileUploader = (id, op) => {
+function fileUploader(id, op) {
     const el = document.getElementById(id);
+    const mainDiv = document.createElement('div');
+    mainDiv.setAttribute('class', 'esay-uploader-main');
+    el.appendChild(mainDiv);
+
     const input = document.createElement('input');
     input.setAttribute('type', 'file');
-    input.setAttribute('id', 'de-uploader');
-    el.appendChild(input);
+    input.setAttribute('class', 'esay-uploader-input');
+    input.setAttribute('id', 'esay-uploader');
+    mainDiv.appendChild(input);
     input.addEventListener("change", function (e) {
         const file = e.target.files[0];
         //创建formdata对象
@@ -19,5 +24,11 @@ window.fileUploader = (id, op) => {
         }
         //将formdata上传
         xhr.send(formdata);
-    })
+    });
+
+    const button = document.createElement('div');
+    button.setAttribute('class', 'esay-uploader-button');
+    button.innerHTML = op.buttonText || '上传文件';
+    mainDiv.appendChild(button);
+    // this.success = () => op.success(xhr);
 }
